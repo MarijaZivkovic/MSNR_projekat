f5c03c0f2777b65dee43de10f314c3d2af3d853d
MilomirRadojevic <milomir993@gmail.com>
2016-04-21 22:47:08 +0200

    Implemented input processing and added buttons to main menu

--- GameSurface.java
+++ GameSurface.java
@@ -1,23 +1,20 @@
 package com.example.milomir93.battleships;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GameSurface extends SurfaceView implements SurfaceHolder.Callback{
+    private enum State {MENU_STATE, LEVEL_STATE};
+    public enum MethodName {CREATE_GAME, JOIN_GAME, START_GAME, EXIT_GAME}
     public static final int WIDTH = 480;
     public static final int HEIGHT = 800;
-    private static int x1 = 0, y1 = 0, x2 = 140, y2 = 350;//change
-    private boolean gameMode = false;//change
-    private Bitmap menuBackground;//change
-    private Bitmap gameBackground;//change
-    private Bitmap newGameButton;//change
     private GameThread thread;
+    private State state;
     private MainMenu mainMenu;
+    private Level level;
 
     public GameSurface(Context context) {
         super(context);
@@ -27,20 +24,14 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback{
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        mainMenu = new MainMenu();
-
-        menuBackground = BitmapFactory.decodeResource(getResources(), R.drawable.g);//change
-        newGameButton = BitmapFactory.decodeResource(getResources(), R.drawable.r);//change
-        gameBackground = BitmapFactory.decodeResource(getResources(), R.drawable.b);//change
-
+        state = State.MENU_STATE;
+        mainMenu = new MainMenu(this);
         thread = new GameThread(getHolder(), this);
         thread.start();
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
-
-    }
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){}
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder){
@@ -55,36 +46,76 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback{
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if(event.getAction() == MotionEvent.ACTION_DOWN) {//change
-            if(event.getX() >= 140 && event.getX() <= 340 && event.getY() >= 350 && event.getY() <= 450)
-                gameMode = true;
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            switch (state) {
+                case MENU_STATE:
+                    mainMenu.inputActionDown((int) event.getX(), (int) event.getY());
+                case LEVEL_STATE:
+                    //
+                    return true;
+            }
+        }
+
+        else if(event.getAction() == MotionEvent.ACTION_MOVE) {
+            switch (state) {
+                case MENU_STATE:
+                    mainMenu.inputActionMove((int) event.getX(), (int) event.getY());
+                    return true;
+                case LEVEL_STATE:
+                    //
+                    return true;
+            }
+        }
+
+        else if(event.getAction() == MotionEvent.ACTION_UP) {
+            switch (state) {
+                case MENU_STATE:
+                    mainMenu.inputActionUp((int) event.getX(), (int) event.getY());
+                    return true;
+                case LEVEL_STATE:
+                    //
+                    return true;
+            }
         }
 
         return super.onTouchEvent(event);
     }
 
     public void update() {
-        //
+        switch (state) {
+            case MENU_STATE:
+                mainMenu.update();
+                break;
+            case LEVEL_STATE:
+                //
+                break;
+        }
     }
 
     @Override
     public void draw(Canvas canvas) {
         final float scaleFactorX = getWidth() / (WIDTH * 1.f);
         final float scaleFactorY = getHeight() / (HEIGHT * 1.f);
-        System.out.println(canvas.getWidth());
 
-        if(canvas!=null) {
+        if(canvas != null) {
             final int savedState = canvas.save();
             canvas.scale(scaleFactorX, scaleFactorY);
 
-            if(!gameMode) {//change
-                canvas.drawBitmap(menuBackground, x1, y1, null);
-                canvas.drawBitmap(newGameButton, x2, y2, null);
-            } else
-                canvas.drawBitmap(gameBackground, x1, y1, null);
+            switch (state) {
+                case MENU_STATE:
+                    mainMenu.draw(canvas);
+                    break;
+                case LEVEL_STATE:
+                    //
+                    break;
+            }
 
             canvas.restoreToCount(savedState);
 
         }
+    }
+
+    void setLevel(Level l) {
+        level = l;
     }
 }
--- MainMenu.java
+++ MainMenu.java
@@ -1,21 +1,102 @@
 package com.example.milomir93.battleships;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 
-public class MainMenu {
-    private GameButton newGame;
-    private GameButton joinGame;
-    private GameButton exitGame;
+import java.util.HashSet;
 
-    public MainMenu() {
-        //
+public class MainMenu {
+    private GameSurface v;
+    private HashSet<GameButton> buttons;
+    private GameButton lastChanged;
+    private Bitmap background;
+    private int x = 0;
+    private int y = 0;
+    private GameButton newGameButton;
+    private GameButton joinGameButton;
+    private GameButton exitGameButton;
+
+    public MainMenu(GameSurface v) {
+        this.v = v;
+        lastChanged = new GameButton();
+        buttons = new HashSet<GameButton>();
+        background = BitmapFactory.decodeResource(v.getResources(), R.drawable.menu_bg);
+        newGameButton = new GameButton(90, 200, v, GameSurface.MethodName.CREATE_GAME);
+        joinGameButton = new GameButton(90, 350, v, GameSurface.MethodName.JOIN_GAME);
+        exitGameButton = new GameButton(90, 500, v, GameSurface.MethodName.EXIT_GAME);
+        buttons.add(newGameButton);
+        buttons.add(joinGameButton);
+        buttons.add(exitGameButton);
+    }
+
+    public void inputActionDown(int x, int y) {
+        for(GameButton g : buttons)
+            if(y >= g.getY() && y <= g.getY() + g.getBackground().getHeight())
+                if(x >= g.getX() && x <= g.getX() + g.getBackground().getWidth()) {
+                    g.setIsWide(GameButton.WIDE);
+                    lastChanged = g;
+                    break;
+                }
+    }
+
+    public void inputActionMove(int x, int y) {
+        lastChanged.setIsWide(GameButton.NON_WIDE);
+        for(GameButton g : buttons)
+            if(y >= g.getY() && y <= g.getY() + g.getBackground().getHeight())
+                if(x >= g.getX() && x <= g.getX() + g.getBackground().getWidth()) {
+                    g.setIsWide(GameButton.WIDE);
+                    lastChanged = g;
+                    break;
+                }
+    }
+
+    public void inputActionUp(int x, int y) {
+        for(GameButton g : buttons)
+            if(y >= g.getY() && y <= g.getY() + g.getBackground().getHeight())
+                if(x >= g.getX() && x <= g.getX() + g.getBackground().getWidth()) {
+                    g.setIsWide(GameButton.NON_WIDE);
+                    lastChanged = g;
+                    switch (g.getMethodName()) {
+                        case CREATE_GAME:
+                            createGame();
+                            break;
+                        case JOIN_GAME:
+                            joinGame();
+                            break;
+                        case EXIT_GAME:
+                            exitGame();
+                            break;
+                    }
+                    break;
+                }
     }
 
     public void update() {
-        //
+        newGameButton.update();
+        joinGameButton.update();
+        exitGameButton.update();
     }
 
     public void draw(Canvas canvas) {
-        //
+        canvas.drawBitmap(background, x, y, null);
+        newGameButton.draw(canvas);
+        joinGameButton.draw(canvas);
+        exitGameButton.draw(canvas);
+    }
+
+    void createGame() {
+        System.out.println("Create");
+        //privremeno
+        Level l = new Level(new Human(), new Bot(), v);
+        v.setLevel(l);
+    }
+
+    void joinGame() {
+        System.out.println("Join");
+    }
+
+    void exitGame() {
+        System.out.println("Exit");
     }
 }
